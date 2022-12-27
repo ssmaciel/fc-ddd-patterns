bb4f09c6320cbfa0153c0450bbe7d7a4dc7521ef
Samuel Maciel <ssmaciel_rp@hotmail.com>
2022-12-27 15:52:42 -0300

    desenv - cap repositories

---
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -0,0 +1,24 @@
+import { Sequelize } from "sequelize-typescript";
+import ProductModel from "../db/sequelize/model/product.model";
+
+describe("Product repostory test", () => {
+
+    let sequelize: Sequelize;
+
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: "sqlite",
+            storage: "memory",
+            logging: false,
+            sync: { force: true }
+        });
+        
+        sequelize.addModels([ProductModel]);
+        await sequelize.sync();
+
+    })
+    
+    afterEach(async () => {
+        await sequelize.close();
+    })
+});
